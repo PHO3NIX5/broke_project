d468c6bc975962ecc50df2ff754e8acb8bc9e4e6
PHO3NIX5 <92304496+PHO3NIX5@users.noreply.github.com>
2022-09-01 17:05:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ var recognition=new speech_rec();
 draw_square="";
 draw_circle="";
 draw_rectangle="";
+x=0;
+y=0;
+length=0;
+width=0;
 function preload(){
 
 }
@@ -11,20 +15,35 @@ function setup(){
     canvas.center();
 }
 function draw(){
+    // fill(random(0,255),random(0,255),random(0,255));
+    // if(draw_circle=="set"){
+    //     circle(random(10,1100),random(10,500),random(10,100));
+    // }
+    // if(draw_square=="set"){
+    //     square(random(10,1100),random(10,500),random(10,100));
+    // }
+    // if(draw_rectangle=="set"){
+    //     rect(random(10,1100),random(10,500),random(10,100),random(10,100));
+    // }
     if(draw_circle=="set"){
-        circle(77,64,40);
+        circle(x,y,length)
     }
-    if(draw_square=="set"){
-        square(90,100,89);
+    if(draw_circle=="set"){
+        square(x,y,length)
     }
-    if(draw_rectangle==set){
-        rect(300,400,789,678)
+    if(draw_rectangle=="set"){
+        rect(x,y,length,breadth)
     }
 }
 function start(){
     recognition.start();
     document.getElementById("status").innerHTML="System is Listening";
+x=random(10,1100);
+y=random(10,500);
+length=random(10,100)
+breadth=random(10,100)
 }
+
 recognition.onresult=function(event){
     console.log(event);
     contents=event.results[0][0].transcript;
@@ -37,7 +56,7 @@ recognition.onresult=function(event){
     if(contents=="rectangle"){
         draw_rectangle="set";
     }
-    if(contents="circle"){
+    if(contents=="circle"){
         draw_circle="set";
     }
     
